e741c892c3de88cd0d598189a0f8b1c80f28a013
al221910432 <al221910432@gmail.com>
2021-11-03 13:10:38 -0600

    video 24

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -7,6 +7,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   String _nombre = '';
+  String _email = '';
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,15 @@ class _InputPageState extends State<InputPage> {
       ),
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-        children: <Widget>[_crearInput(), Divider(), _crearPersona()],
+        children: <Widget>[
+          _crearInput(),
+          Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
+          Divider(),
+          _crearPersona()
+        ],
       ),
     );
   }
@@ -41,9 +50,40 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
+  Widget _crearEmail() {
+    return TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+            hintText: 'Email',
+            labelText: 'Email',
+            suffixIcon: Icon(Icons.alternate_email),
+            icon: Icon(Icons.email)),
+        onChanged: (valor) => setState(() {
+              _email = valor;
+            }));
+  }
+
+  Widget _crearPassword() {
+    return TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+            hintText: 'Password',
+            labelText: 'Password',
+            suffixIcon: Icon(Icons.lock_open),
+            icon: Icon(Icons.lock)),
+        onChanged: (valor) => setState(() {
+              _email = valor;
+            }));
+  }
+
   Widget _crearPersona() {
     return ListTile(
       title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email: $_email'),
     );
   }
 }
